bf668c00c4242adeb3f6e19760798a2092dc1daf
Archita bansal <architabansall05@gmail.com>
2025-06-29 19:13:17 +0530

    reinitialise project structure

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Welcome to Next.js!</h1>
+      <h1 className="text-3xl font-bold underline">Welcome to MedGency</h1>
     </div>
   );
 }
